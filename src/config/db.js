deeb2d7d70b88fc706f2b6c476d5ddf729d68e03
LegendDev14 <devendrapratap992@gmail.com>
2026-08-20 11:06:29 +0530

    update db

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ import env from "./env.js";
 
 const connectDatabase = async () => {
   if (!env.mongoUri) return;
+  if (mongoose.connection.readyState === 1) return;
   await mongoose.connect(env.mongoUri);
   console.log("MongoDB connected");
 };
